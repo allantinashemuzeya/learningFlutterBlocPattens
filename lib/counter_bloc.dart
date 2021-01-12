60697873cb070719914bc317e200e8fc4bf53d8c
allanthecodemaster <allan.thecodemaster@gmail.com>
2021-01-13 00:11:32 +0200

    Bloc Patterns tutorial knowledge two --understood better

--- lib/counter_bloc.dart
+++ lib/counter_bloc.dart
@@ -1,41 +1,83 @@
+// import 'dart:async';
+//
+// import 'package:flutter_bloc_patterns/counter_event.dart';
+//
+// //Bloc stands for Business Logic Component
+// // this means that the business logic of the App should be separate from the UI
+// // Bloc is used for state management.
+//
+// class CounterBloc {
+//   int _counter = 0;
+//
+//   final _counterStateController = StreamController<int>();
+//
+//   StreamSink<int> get _inCounter => _counterStateController.sink;
+//
+// //  For state, exposing only a stream which outputs data
+//   Stream<int> get counter => _counterStateController.stream;
+//
+//   final _counterEventController = StreamController<CounterEvent>();
+//   // Fore events, exposing only a sink which is an input
+//   Sink<CounterEvent> get counterEventSink => _counterEventController.sink;
+//
+//   CounterBloc(){
+//     // Whenever there is a new event, we want to map it to a new state
+//     _counterEventController.stream.listen(_mapEventToState);
+//   }
+//
+//   void _mapEventToState(CounterEvent event){
+//     if (event is IncrementEvent)
+//       _counter++;
+//     else
+//       _counter--;
+//
+//     _inCounter.add(_counter);
+//   }
+//
+//   void dispose(){
+//     _counterStateController.close();
+//     _counterEventController.close();
+//   }
+// }
+
 import 'dart:async';
 
-import 'package:flutter_bloc_patterns/counter_event.dart';
-
-//Bloc stands for Business Logic Component
-// this means that the business logic of the App should be separate from the UI
-// Bloc is used for state management.
+enum CounterAction{
+  Increment,
+  Decrement,
+  Reset
+}
 
 class CounterBloc {
-  int _counter = 0;
 
-  final _counterStateController = StreamController<int>();
 
-  StreamSink<int> get _inCounter => _counterStateController.sink;
+int _counter = 0;
+//  What goes in is called a SINK (input)
+// What comes out is called Stream (State Stream Controller)
 
-//  For state, exposing only a stream which outputs data
-  Stream<int> get counter => _counterStateController.stream;
+  final _stateStreamController = StreamController<int>();
 
-  final _counterEventController = StreamController<CounterEvent>();
-  // Fore events, exposing only a sink which is an input
-  Sink<CounterEvent> get counterEventSink => _counterEventController.sink;
+  StreamSink<int> get counterSink => _stateStreamController.sink; // input
+  Stream<int> get counterStream => _stateStreamController.stream; // output
+
+  final _eventStreamController = StreamController<CounterAction>();
+
+  StreamSink<CounterAction> get eventSink => _eventStreamController.sink;
+  Stream<CounterAction> get eventStream => _eventStreamController.stream;
 
   CounterBloc(){
-    // Whenever there is a new event, we want to map it to a new state
-    _counterEventController.stream.listen(_mapEventToState);
-  }
+    eventStream.listen((event)=>{
+      if(event == CounterAction.Increment) _counter++
+      else if(event == CounterAction.Decrement) _counter--
+      else if(event == CounterAction.Reset) _counter = 0,
 
-  void _mapEventToState(CounterEvent event){
-    if (event is IncrementEvent)
-      _counter++;
-    else
-      _counter--;
-
-    _inCounter.add(_counter);
+      counterSink.add(_counter)
+    });
   }
 
   void dispose(){
-    _counterStateController.close();
-    _counterEventController.close();
+    _stateStreamController.close();
+    _eventStreamController.close();
   }
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -25,7 +24,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-
   final String title;
 
   @override
@@ -33,48 +31,61 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-  final _bloc = CounterBloc();
-
+  final _counterBloc = CounterBloc();
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
         title: Text(widget.title),
       ),
       body: Center(
-
-        child: StreamBuilder(
-          stream: _bloc.counter,
-          initialData: 0,
-          builder: (BuildContext context, AsyncSnapshot<int> snapshot){
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text('You have pushed the button this many times'),
-                Text('${snapshot.data}', style: Theme.of(context).textTheme.headline4,)
-              ],
-            );
-          }
-        )
-      ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          FloatingActionButton(
-            onPressed: () => _bloc.counterEventSink.add(IncrementEvent()),
-            tooltip: 'Increment',
-            child: Icon(Icons.add),
-          ),
-          SizedBox(width: 10),
-          FloatingActionButton(
-            onPressed: () => _bloc.counterEventSink.add(DecrementEvent()),
-            tooltip: 'Decrement',
-            child: Icon(Icons.remove),
-          ),
+          Text('You have pushed the button this many times'),
+          StreamBuilder(
+              stream: _counterBloc.counterStream,
+              initialData: 0,
+              builder: (context, snapshot) {
+                return Text(
+                  '${snapshot.data}',
+                  style: Theme.of(context).textTheme.headline4,
+                );
+              })
+        ],
+      )),
+      floatingActionButton:
+          Row(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
+        FloatingActionButton(
+          onPressed: () => {
+            _counterBloc.eventSink.add(CounterAction.Reset),
+          },
+          tooltip: 'Reset',
+          child: Icon(Icons.loop),
+        ),
+        SizedBox(width: 10),
+        FloatingActionButton(
+          onPressed: () => {
+            _counterBloc.eventSink.add(CounterAction.Decrement),
+          },
+          tooltip: 'Decrement',
+          child: Icon(Icons.remove),
+        ),
+        SizedBox(width: 10),
+        FloatingActionButton(
+          onPressed: () => {
+            _counterBloc.eventSink.add(CounterAction.Increment),
+          },
+          tooltip: 'Increment',
+          child: Icon(Icons.add),
+        ),
+        SizedBox(width: 10),
+        // FloatingActionButton(
+        // onPressed: () => _bloc.counterEventSink.add(DecrementEvent()),
+        // tooltip: 'Decrement',
+        // child: Icon(Icons.remove),
+        // ),
       ]), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
